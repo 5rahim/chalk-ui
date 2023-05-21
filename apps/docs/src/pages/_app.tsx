8f96f6b1a6ce13a91e9520d94360028d70d93f42
5rahim <bonfohzaki@gmail.com>
2023-05-21 17:38:16 -0400

    fix: build

--- apps/docs/src/pages/_app.tsx
+++ apps/docs/src/pages/_app.tsx
@@ -1,18 +1,12 @@
 import type { AppProps } from "next/app"
-import { customizeStyleLibrary, UIProvider } from "ui"
+import { UIProvider } from "ui"
 import "../../styles.css"
 import "../styles/globals.css"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
    
    return (
-      <UIProvider
-         styleLibrary={customizeStyleLibrary(({ extendAnatomy, part }) => ({
-            Switch: extendAnatomy("Switch", {
-               label: part("relative font-medium data-disabled:text-gray-300"),
-            }),
-         }))}
-      >
+      <UIProvider>
          <Component {...pageProps} />
       </UIProvider>
    )
--- packages/ui/src/calendar/locale.ts
+++ packages/ui/src/calendar/locale.ts
@@ -1,4 +1,4 @@
-import enUS from "date-fns"
-import fr from "date-fns"
+import enUS from "date-fns/locale/en-US"
+import fr from "date-fns/locale/fr"
 
 export const getDateLocaleLibrary = (locale: string) => locale.includes("fr") ? fr : enUS
