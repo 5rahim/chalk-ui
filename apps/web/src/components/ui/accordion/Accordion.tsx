f4e770198065be2ea0d7c9991fa7a5f7c5ff8f8b
5rahim <bonfohzaki@gmail.com>
2023-05-22 11:22:28 -0400

    chalk: install dependencies

---
+++ apps/web/src/components/ui/accordion/Accordion.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import React from "react"
+
+interface AccordionProps {
+   children?: React.ReactNode
+}
+
+export const Accordion: React.FC<AccordionProps> = (props) => {
+   
+   const { children, ...rest } = props
+   
+   return (
+      <>
+      
+      </>
+   )
+   
+}
