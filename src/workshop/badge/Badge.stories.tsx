b6719030f9d5d5f8283c231841aaf1d1679db240
5rahim <bonfohzaki@gmail.com>
2024-01-06 16:05:54 -0500

    badge stories

---
+++ src/workshop/badge/Badge.stories.tsx
@@ -0,0 +1,78 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { Badge } from "../badge"
+
+const meta = {
+    title: "Components/DataDisplay/Badge",
+    component: Badge,
+    tags: ["autodocs"],
+} satisfies Meta<typeof Badge>
+
+
+export default meta
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+    args: {
+        children: "Badge",
+    },
+}
+export const Large: Story = {
+    args: {
+        size: "lg",
+        children: "Badge",
+    },
+}
+
+export const XLarge: Story = {
+    args: {
+        size: "xl",
+        children: "Badge",
+    },
+}
+export const Small: Story = {
+    args: {
+        size: "sm",
+        children: "Badge",
+    },
+}
+
+export const Colors: Story = {
+    render: () => (
+        <div className="flex flex-row gap-2 flex-wrap">
+            <Badge intent="primary">Badge</Badge>
+            <Badge intent="primary-solid">Badge</Badge>
+
+            <Badge intent="alert">Badge</Badge>
+            <Badge intent="alert-solid">Badge</Badge>
+
+            <Badge intent="success">Badge</Badge>
+            <Badge intent="success-solid">Badge</Badge>
+
+            <Badge intent="warning">Badge</Badge>
+            <Badge intent="warning-solid">Badge</Badge>
+
+            <Badge intent="gray">Badge</Badge>
+            <Badge intent="gray-solid">Badge</Badge>
+
+            <Badge intent="blue">Badge</Badge>
+            <Badge intent="blue-solid">Badge</Badge>
+
+            <Badge intent="white">Badge</Badge>
+            <Badge intent="white-solid">Badge</Badge>
+        </div>
+    ),
+}
+
+export const Tag: Story = {
+    args: {
+        tag: true,
+        children: "Badge",
+    },
+}
+export const ClosableTag: Story = {
+    args: {
+        tag: true,
+        isClosable: true,
+        children: "Badge",
+    },
+}
