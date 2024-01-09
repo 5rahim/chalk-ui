5be1fd52f0c481d5ade0a25c864672aafa1f3f3c
5rahim <bonfohzaki@gmail.com>
2024-01-09 15:41:55 -0500

    stats

---
+++ src/workshop/stats/Stats.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { BiBarChart, BiLineChart, BiMouseAlt } from "react-icons/bi"
+import { Stats } from "."
+
+const meta = {
+    title: "Components/Data Display/Stats",
+    component: Stats,
+    tags: ["autodocs"],
+    render: (args) => {
+        return (
+            <Stats {...args} />
+        )
+    },
+    args: {
+        items: [
+            {
+                icon: <BiBarChart />,
+                name: "Total Subscribers",
+                value: "71,897",
+                unit: "70,946",
+                change: "12%",
+                trend: "up",
+            },
+            {
+                icon: <BiLineChart />,
+                name: "Avg. Open Rate",
+                value: "56.16%",
+                unit: "58.14%",
+                change: "2.02%",
+                trend: "down",
+            },
+            {
+                icon: <BiMouseAlt />,
+                name: "Avg. Click Rate",
+                value: "24.57%",
+            },
+        ],
+        className: "border border-[--border]",
+    },
+} satisfies Meta<typeof Stats>
+
+
+export default meta
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+    args: {},
+}
