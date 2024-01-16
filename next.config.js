0a8f98b12b07697f7ec66f5a3efdc5a5e449bf61
5rahim <bonfohzaki@gmail.com>
2024-01-16 15:10:50 -0500

    update next config

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const { withContentlayer } = require("next-contentlayer");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        unoptimized: true
+    }
+}
+
+module.exports = withContentlayer(nextConfig)
